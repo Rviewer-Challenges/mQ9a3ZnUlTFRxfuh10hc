7925b0b20382c26150c1340d814cd79ecb5b1b41
gracebir <grace.devolop@gmail.com>
2023-10-17 14:06:56 +0200

    changing modal layout background color and opacity

--- memory-game-app/src/app/_components/ModalLayout.tsx
+++ memory-game-app/src/app/_components/ModalLayout.tsx
@@ -6,8 +6,8 @@ type modalProp = {
 
 const ModalLayout: FC<modalProp> = ({children}) => {
   return (
-    <div className='absolute w-full right-0 left-0 h-screen inset-0 backdrop-blur-sm flex items-center'>
-      <div className='bg-white max-w-4xl mx-auto rounded-lg shadow-lg flex-1 px-6 lg:px-8 lg:py-7 py-5'>
+    <div className='absolute w-full right-0 top-0 left-0 h-screen bg-black bg-opacity-75 lg:px-0 px-6 flex items-center'>
+      <div className='bg-white max-w-2xl mx-auto rounded-lg shadow-lg flex-1 px-6 lg:px-8 lg:py-7 py-5'>
         {children}
       </div>
     </div>
