aff0d42a11c84c1a69c962087424f3c0cd0e5c19
gracebir <grace.devolop@gmail.com>
2023-10-17 08:23:43 +0200

    game circle card

--- memory-game-app/src/app/_components/CardCircle.tsx
+++ memory-game-app/src/app/_components/CardCircle.tsx
@@ -1,24 +1,29 @@
 'use client'
-import classNames from 'classnames'
-import React, { FC, ReactElement } from 'react'
+import classNames from 'classnames';
+import React, { FC } from 'react';
 
-type cardCircleProps = {
-    icon: ReactElement
-    selected: boolean
-    disabled: boolean
-    gridSize: number
-}
+type CardCircleProps = {
+    icon: React.ReactNode;
+    gridSize: number;
+    visible: boolean; // Pass the visibility state as a prop
+    onClick: () => void;
+};
 
-const CardCircle: FC<cardCircleProps> = ({ selected, disabled, icon, gridSize }) => {
+const CardCircle: FC<CardCircleProps> = ({ icon, gridSize, visible, onClick }) => {
+    const handleClick = () => {
+        if (!visible) {
+            onClick(); // Notify the parent component of the card click
+        }
+    };
+
     return (
         <div
             className={classNames(
                 'text-white cursor-pointer rounded-full flex justify-center items-center transition ease-in delay-150 transform hover:translate-x-1',
                 {
-                    'pointer-events-none': selected,
-                    'bg-navy-blue': !selected,
-                    'bg-light-gray': selected && disabled,
-                    'bg-orange-color': selected && !disabled,
+                    'bg-navy-blue': !visible,
+                    'bg-light-gray': visible,
+                    'bg-orange-color': visible,
                 }
             )}
             style={{
@@ -26,16 +31,19 @@ const CardCircle: FC<cardCircleProps> = ({ selected, disabled, icon, gridSize })
                 height: gridSize === 6 ? '46px' : '65px',
                 fontSize: gridSize === 6 ? '24px' : '40px',
             }}
+            onClick={handleClick} // Handle card click to reveal it
         >
-            <button
-                className={classNames({
-                    hidden: !selected,
-                })}
-            >
-                {icon}
-            </button>
+            {visible && (
+                <button
+                    onClick={(e) => {
+                        e.stopPropagation();
+                    }}
+                >
+                    {icon}
+                </button>
+            )}
         </div>
-    )
-}
+    );
+};
 
-export default CardCircle
+export default CardCircle;
