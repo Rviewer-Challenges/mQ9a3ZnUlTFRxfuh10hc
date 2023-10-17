9e08d23fdfcf1551c1e5c4c82294684447649c92
gracebir <grace.devolop@gmail.com>
2023-10-17 08:22:36 +0200

    handle new game button

--- memory-game-app/src/app/_components/Header.tsx
+++ memory-game-app/src/app/_components/Header.tsx
@@ -1,15 +1,20 @@
 'use client'
 import React from 'react'
 import Button from './Elements/Button'
+import { useRouter } from 'next/navigation'
 
 const Header = () => {
+    const router = useRouter()
+    const handleNewGame = () => {
+        router.push('/')
+    }
     return (
         <header className="py-6 lg:py-10">
             <div className='container mx-auto flex lg:justify-between gap-4 items-center'>
                 <h1 className="text-3xl font-bold lg:text-4xl">memory</h1>
                 <nav className='flex flex-row gap-4 lg:gap-6'>
                     <Button isActive={true} text='Restart' handleClick={() => { }} />
-                    <Button isActive={false} text='New Game' handleClick={() => { }} />
+                    <Button isActive={false} text='New Game' handleClick={handleNewGame} />
                 </nav>
             </div>
         </header>
