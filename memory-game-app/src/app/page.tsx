ec56fc94a0f24c769615157fe821accfb531fb80
gracebir <grace.devolop@gmail.com>
2023-10-17 14:12:22 +0200

    addding padding in x absis

--- memory-game-app/src/app/page.tsx
+++ memory-game-app/src/app/page.tsx
@@ -2,7 +2,7 @@ import ChooseLevel from "./_components/ChooseLevel";
 
 export default function Home() {
   return (
-    <main className="h-screen w-full bg-dark-blue flex items-center">
+    <main className="h-screen w-full bg-dark-blue flex items-center px-6 lg:px-0">
       <ChooseLevel/>
     </main>
   )
