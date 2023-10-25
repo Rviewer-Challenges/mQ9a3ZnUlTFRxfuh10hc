8b623e8e92c83ba09f6c92074c2f57a6577f8c0d
gracebir <grace.devolop@gmail.com>
2023-10-25 14:22:41 +0200

    moving actions types in type file

--- memory-game-app/src/utils/type.d.ts
+++ memory-game-app/src/utils/type.d.ts
@@ -3,6 +3,7 @@ export type GameState = {
     gameBoard: CardData[];
     moveCount: number;
     timeLeft: number;
+    size: number;
     remainingPairs: number;
     selectedCards: number[];
 };
@@ -11,3 +12,12 @@ export type CardData = {
     icon: JSX.Element;
     value: string;
 };
+
+type Action =
+    | { type: 'SELECT_LEVEL'; payload: string }
+    | { type: 'INCREMENT_MOVE_COUNT' }
+    | { type: 'DECREMENT_TIME' }
+    | { type: 'DECREMENT_REMAINING_PAIRS' }
+    | { type: 'SELECT_CARD'; payload: number }
+    | { type: 'RESTART_GAME' }
+    | { type: 'CHECK_MATCH' };
